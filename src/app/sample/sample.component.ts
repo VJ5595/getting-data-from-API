15e2bef616f590efc0c116241527deab692de5a4
Vijay <vijay.r@adappt.co.in>
2018-07-02 18:18:50 +0530

    added debounce timing for searching query

--- src/app/sample/sample.component.ts
+++ src/app/sample/sample.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { SampleService } from './sample.service.js';
 import * as Configuration from './../../../config.js';
+
 @Component({
   selector: 'app-sample',
   templateUrl: './sample.component.html',
@@ -13,19 +14,49 @@ export class SampleComponent implements OnInit {
   public articleList = [];
   public pagesize = 10;
   public configApi = Configuration;
+  public range : any;
 
-  constructor(private _sampleService: SampleService) { }
+  constructor(private _sampleService: SampleService) {}
 
   ngOnInit() {
-    this.onApicall(1);
   }
+
+  public debounce(func, wait, immediate=null) {
+
+    var timeout;
+    return function() {
+      var context = this, args = arguments;
+      var later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  };
+  
+  saverange(newValue) {
+    this.debounce(()=> {
+      this.range = newValue;
+      if (newValue.length > 3) {
+        this.onApicall(1);
+     }
+    }, 2500)();
+    
+   
+  } 
   onApicall(page) {
-    var query = "&page=" + page + "&pageSize=" + this.pagesize + "&apiKey=" + this.configApi.apikey;
+    var query = "&q=" + this.range + "&page=" + page + "&pageSize=" + this.pagesize + "&apiKey=" + this.configApi.apikey;
+    console.log("++++++++++++++++++",query);
     this._sampleService.getCount(query)
+
       .subscribe((data1) => {
         this.count = data1['totalResults'];
         this.p = page; 
         this.articleList = data1['articles'];
+        console.log("articles",data1['articles']);
       })
   }
 
--- src/app/sample/sample.service.ts
+++ src/app/sample/sample.service.ts
@@ -9,6 +9,6 @@ export class SampleService {
   constructor(private _http: HttpClient) { }
 
   getCount(query) {
-    return this._http.get("https://newsapi.org/v2/everything?q=apple&from=2018-06-27&to=2018-06-27&sortBy=popularity"+ query);
+    return this._http.get("https://newsapi.org/v2/everything?sortBy=relevancy"+ query);
   }
 }
